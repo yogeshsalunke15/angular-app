adcdb6c9998c23bdaadbee610e4be928b2b20c0f
yogeshsalunke15@gmail.com <yogeshsalunke15@gmail.com>
2016-08-10 19:56:46 +0530

    added left side panel css & main structure

--- app/js/directives/left-nav.js
+++ app/js/directives/left-nav.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('demoApp').
+	directive('headerNav', function () {
+		return {
+			restrict: 'EA',
+			replace: true,
+            transclude: true,
+            controller: 'NavCtrl as nav',
+    		templateUrl: 'templates/header.html'
+  		};
+	});
+	
+angular.module('demoApp').
 	directive('leftNav', function () {
 		return {
 			restrict: 'EA',
